f02e3a9e9c173dc44add560226200d3b847cac6c
bams-dev <bambainza91@gmail.com>
2026-05-14 11:23:52 +0000

    feat(books): implement CRUD operations

---
+++ books-service/src/controllers/bookController.js
@@ -0,0 +1,36 @@
+const Book = require('../models/Book');
+
+exports.createBook = async (req, res) => {
+  const book = await Book.create(req.body);
+  res.status(201).json(book);
+};
+
+exports.getBooks = async (req, res) => {
+  const books = await Book.findAll();
+  res.json(books);
+};
+
+exports.getBook = async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  res.json(book);
+};
+
+exports.updateBook = async (req, res) => {
+
+  const book = await Book.findByPk(req.params.id);
+
+  await book.update(req.body);
+
+  res.json(book);
+};
+
+exports.deleteBook = async (req, res) => {
+
+  const book = await Book.findByPk(req.params.id);
+
+  await book.destroy();
+
+  res.json({
+    message: 'Book deleted'
+  });
+};
