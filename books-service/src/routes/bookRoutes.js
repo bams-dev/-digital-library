50b796aa495e6bd8a9045a4e9e43c4b24f88def4
bams-dev <bambainza91@gmail.com>
2026-05-14 11:25:02 +0000

    feat(books): add API routes

---
+++ books-service/src/routes/bookRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {
+  createBook,
+  getBooks,
+  getBook,
+  updateBook,
+  deleteBook
+} = require('../controllers/bookController');
+
+router.post('/', createBook);
+router.get('/', getBooks);
+router.get('/:id', getBook);
+router.put('/:id', updateBook);
+router.delete('/:id', deleteBook);
+
+module.exports = router;
